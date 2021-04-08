122ea2119745c87fb81bf4027e8649e906219b17
Alex Kiura <kiuraalex@gmail.com>
2021-04-08 08:50:39 +0300

    Define variables for web module

---
+++ modules/web/variables.tf
@@ -0,0 +1,50 @@
+variable "region" {
+  description = "The region to launch the instances"
+}
+
+variable "max_az_count" {
+  default = 2
+}
+
+variable "instance_type" {
+  description = "The instance type to launch"
+}
+
+
+variable "vpc_sg_id" {
+  description = "The default security group from the vpc"
+}
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block from the VPC"
+}
+
+variable "key_name" {
+  description = "The keypair to use on the instances"
+}
+
+variable "environment" {
+  description = "The environment for the instance"
+  default = "staging"
+}
+
+variable "vpc_id" {
+  description = "The id of the vpc"
+}
+
+variable "private_subnet_ids" {
+  description = "The list of private subnet ids"
+}
+
+variable "public_subnet_ids" {
+  description = "The list of private subnet ids"
+}
+
+variable "ami_id" {
+  description = "The id of the AMI"
+}
+
+variable "availability_zones" {
+  description = "The availability zones available in a region"
+}
+
